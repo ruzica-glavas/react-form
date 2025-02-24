9db730b1883462729bd310088526b0bad2da130a
ruzica-glavas <ruzi99@hotmail.it>
2025-02-24 17:10:22 +0100

    add: comments, placeholder in input and finish the submit event

--- src/App.jsx
+++ src/App.jsx
@@ -4,45 +4,50 @@ const list = [`Pane`, `Formaggio`, `Latte`]
 
 function App() {
 
-  const [addArticle, setAddArticle] = useState (list)
-  const [newList, setNewList] = useState(``)
+  const [items, setItems] = useState (list)
+  const [newItem, setNewItem] = useState(``)
 
   const addItem =e => {
     e.preventDefault();
-    const article = newList.trim();
-    setAddArticle([...addArticle, newList])
-    setNewList(``)
-    {/*const updateList = [... addArticle, newList];
-    setAddArticle(updateList) */}
+    const article = newItem.trim();
+    setItems([...items, newItem])
+    setNewItem(``)
+    {/*const updateList = [... items, newItem];
+    setItems(updateList) 
+
+    Se lo scrivo in questo modo, nell'input mi rimane l'ultima cosa che ho aggiunto
+    */}
   }
 
-
+{/* Serve per non far ricaricare la pagina */}
  const handleSubmit = event => {
     event.preventDefault();
-    console.log(addArticle)
+    console.log(items)
   }
  
   return (
     <>
       <h1>Lista di Articoli</h1>
    
-      <p>Gli articoli da te inseriti sono: {addArticle}</p>
+      <p>Gli articoli da te inseriti sono:</p>
 
-      {/* Lista dei Task*/}
+      {/* Lista dei Items*/}
 
       <ul>
-          {addArticle.map((element, index )=> (
+          {items.map((element, index )=> (
             <li key={index}>{element}</li>
           ))}     
       </ul>
     
+    {/* Evento submit */}
 
       <p>Inserisci un nuovo articolo:</p>
       <form onSubmit={addItem}>
         <input 
         type="text" 
-        value ={newList}
-        onChange={(e) => {setAddArticle (e.target.value)} }>
+        placeholder="Aggiungi articolo"
+        value ={newItem}
+        onChange={(e) => {setNewItem (e.target.value)}}>
         </input>
         <button>Invia</button>
       </form>
