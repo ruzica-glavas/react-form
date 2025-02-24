8dc0cf13aa75c31b1bcd0296daa9aa03ae3ebf9a
ruzica-glavas <ruzi99@hotmail.it>
2025-02-24 15:30:01 +0100

    add: input

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,21 @@ import { useState } from "react"
 const list = [``]
 
 function App() {
+
+  const [addArticle, setAddArticle] = useState (`Ciao`)
  
   return (
     <>
       <h1>Lista di Articoli</h1>
+
+      <p>Inserisci un nuovo articolo:</p>
+      <input 
+      type="text" 
+      value ={addArticle}
+      onChange={e => {setAddArticle (e.target.value)} }>
+      </input>
+
+      <p>Gli articoli da te inseriti sono: {addArticle}</p>
     
     </>
      
