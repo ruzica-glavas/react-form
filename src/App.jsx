95d288a963b3476def3e51fa91f3d0e1120e39c9
ruzica-glavas <ruzi99@hotmail.it>
2025-02-24 16:43:41 +0100

    add: list

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import { useState } from "react"
 
-const list = [`Pane`, `Latte`, `Formaggio`]
+const list = [`Pane`, `Formaggio`, `Latte`]
 
 function App() {
 
-  const [addArticle, setAddArticle] = useState (`Ciao`)
-  const handleSubmit = event => {
+  const [addArticle, setAddArticle] = useState (list)
+  const [newList, setNewList] = useState(``)
+
+  const addItem =e => {
+    e.preventDefault();
+    const article = newList.trim();
+    setAddArticle([...addArticle, newList])
+    setNewList(``)
+    {/*const updateList = [... addArticle, newList];
+    setAddArticle(updateList) */}
+  }
+
+
+ const handleSubmit = event => {
     event.preventDefault();
     console.log(addArticle)
   }
@@ -13,19 +25,27 @@ function App() {
   return (
     <>
       <h1>Lista di Articoli</h1>
+   
+      <p>Gli articoli da te inseriti sono: {addArticle}</p>
+
+      {/* Lista dei Task*/}
+
+      <ul>
+          {addArticle.map((element, index )=> (
+            <li key={index}>{element}</li>
+          ))}     
+      </ul>
+    
 
       <p>Inserisci un nuovo articolo:</p>
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={addItem}>
         <input 
         type="text" 
-        value ={addArticle}
-        onChange={e => {setAddArticle (e.target.value)} }>
+        value ={newList}
+        onChange={(e) => {setAddArticle (e.target.value)} }>
         </input>
         <button>Invia</button>
       </form>
-
-      <p>Gli articoli da te inseriti sono: {addArticle}</p>
-    
     </>
      
     )
